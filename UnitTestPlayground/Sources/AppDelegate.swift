2414433a97f49a5a9a36178d3756ebe406a43d72
jsryudev <jsryu.dev@gmail.com>
2021-09-21 22:57:58 +0900

    Init

---
+++ UnitTestPlayground/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UnitTestPlayground
+//
+//  Created by JunSang Ryu on 2021/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+}
---
+++ UnitTestPlayground/Sources/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UnitTestPlayground
+//
+//  Created by JunSang Ryu on 2021/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
